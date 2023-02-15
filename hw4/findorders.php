7d2a6f9f048ce1202f7ee0552e193e2fa7febbfe
TINGJUN CHEN <80810449+theonering3@users.noreply.github.com>
2023-02-14 20:40:29 -0800

    Add files via upload

---
+++ hw4/findorders.php
@@ -0,0 +1,77 @@
+<?php
+
+include('connectionData.txt');
+
+$conn = mysqli_connect($server, $user, $pass, $dbname, $port)
+or die('Error connecting to MySQL server.');
+
+?>
+
+<html>
+<head>
+  <title>PHP-MySQL Program</title>
+  </head>
+  
+  <body bgcolor="white">
+  
+  
+  <hr>
+  
+  
+<?php
+  
+$manu = $_POST['manufacturer'];
+
+$manu = mysqli_real_escape_string($conn, $manu);
+// this is a small attempt to avoid SQL injection
+// better to use prepared statements
+
+$query = "SELECT c.fname AS fname,c.lname AS lname,s.description AS ds
+FROM customer c JOIN orders o ON c.customer_num=o.customer_num
+JOIN items i ON o.order_num=i.order_num
+JOIN stock s ON i.stock_num=s.stock_num
+JOIN manufact m ON s.manu_code=m.manu_code
+WHERE m.manu_name=";
+$query = $query."'".$manu."' ORDER BY 2;";
+
+?>
+
+<p>
+The query:
+<p>
+<?php
+print $query;
+?>
+
+<hr>
+<p>
+Result of query:
+<p>
+
+<?php
+$result = mysqli_query($conn, $query)
+or die(mysqli_error($conn));
+
+print "<pre>";
+while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
+  {
+    print "\n";
+    print "Firstname: $row[fname]  Last name: $row[lname]  Item Description: $row[ds]";
+  }
+print "</pre>";
+
+mysqli_free_result($result);
+
+mysqli_close($conn);
+
+?>
+
+<p>
+<hr>
+
+<p>
+ 
+ 
+</body>
+</html>
+	  
